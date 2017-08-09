2c8ae940ff3b2e407deef12519fbe278abb400d6
Will Burnette <will@hsdesignhouse.com>
2017-08-09 11:25:10 -0500

    Complete comments feature

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,9 @@ class RecipesController < ApplicationController
   end
 
   def show
-
-  end
+      @comment = Comment.new
+      @comments = @recipe.comments.paginate(page: params[:page], per_page: 5)
+    end
 
   def new
     @recipe = Recipe.new
